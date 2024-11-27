fb952b48f46f5de0a63007913a28930b13192960
Yunhan Gao <gaoyunhan040702@sina.com>
2024-11-27 10:30:19 +0800

    update

--- 04-implementation-startexample/Event-Managament-System/src/main/java/ucd/comp3013j/ems/websecurity/WebSecurityConfig.java
+++ 04-implementation-startexample/Event-Managament-System/src/main/java/ucd/comp3013j/ems/websecurity/WebSecurityConfig.java
@@ -30,8 +30,9 @@ public class WebSecurityConfig {
                 .requestMatchers("/customer").hasAuthority("CUSTOMER")
                 .requestMatchers("/organiser").hasAuthority("ORGANISER")
                 .requestMatchers("/signup").permitAll()
-                .requestMatchers("/events*").hasAuthority("CUSTOMER")
-                .requestMatchers("/events/available").hasAuthority("CUSTOMER")
+                .requestMatchers("/events/**").hasAnyAuthority("ADMINISTRATOR", "ORGANISER", "CUSTOMER")
+
+                .anyRequest().authenticated()
                 )
             .formLogin((form) -> form.loginPage("/login").loginProcessingUrl("/login")
                 .successHandler(new AuthSuccess())
