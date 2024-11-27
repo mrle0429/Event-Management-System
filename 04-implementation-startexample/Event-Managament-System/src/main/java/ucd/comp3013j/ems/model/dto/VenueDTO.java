35b7682e411bc18d861be7c6a474a2b902ffa236
zihengWang <wangzih2004@126.com>
2024-11-27 20:10:39 +0800

    feat: 修复添加场馆时的表格错误问题

--- 04-implementation-startexample/Event-Managament-System/src/main/java/ucd/comp3013j/ems/model/dto/VenueDTO.java
+++ 04-implementation-startexample/Event-Managament-System/src/main/java/ucd/comp3013j/ems/model/dto/VenueDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ucd.comp3013j.ems.model.enums.TicketType;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -17,5 +18,15 @@ public class VenueDTO {
     private String contactPhone;
     private String contactEmail;
     private String description;
-    private Map<TicketType, Integer> seatsByLevel;
+    private String premium;
+    private String standard;
+    private String economy;
+
+    public Map<TicketType, Integer> getSeatsByLevel() {
+        Map<TicketType, Integer> seats = new HashMap<>();
+        seats.put(TicketType.PREMIUM, Integer.parseInt(premium));
+        seats.put(TicketType.STANDARD, Integer.parseInt(standard));
+        seats.put(TicketType.ECONOMY, Integer.parseInt(economy));
+        return seats;
+    }
 }
